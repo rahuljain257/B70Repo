28e7e4549c6c063856932a6984710cf07c09358f
Rahul Jain <rahuljain257@hotmail.com>
2025-01-19 04:48:28 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/demo/b70sapui5/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comdemo/b70sapui5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
